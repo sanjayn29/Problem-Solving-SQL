7fb3c213b1eb34adaebafbf6426284b9e201d53a
Sanjay N <sanjayn29.aiml@gmail.com>
2026-07-13 08:00:04 +0530

    padi padi di

---
+++ 1164-Product Price at a Given Date.sql
@@ -0,0 +1,5 @@
+SELECT product_id,
+COALESCE( (SELECT new_price FROM Products
+WHERE product_id = P.product_id AND change_date <= '2019-08-16'
+ORDER BY change_date DESC LIMIT 1), 10) AS price
+FROM ( SELECT DISTINCT product_id FROM products ) P;
